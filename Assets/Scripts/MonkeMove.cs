875fee665d2357744f85daf5661fc17bbfaa94af
Hritik Dutta <dutta.hritik@gmail.com>
2021-06-21 02:15:41 +0530

    Implemented A* pathfinding for monkeys so they can find their way around obstacles

--- Assets/Scripts/MonkeMove.cs
+++ Assets/Scripts/MonkeMove.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+// @Watch: https://www.youtube.com/watch?v=CTf0WjhfBx8 for proper top-down sorting
+
 public class MonkeMove : MonoBehaviour
 {
     public Transform bananaYumYum;
     public MonkeSettings settings;
+    public PathFinder finder;
 
+    [Header("Level")]
     public Tilemap groundTilemap;
     public Tilemap levelTilemap;
 
-    private Vector3 targetPosition;
+    [Header("For Debugging REMOVE LATER")]
+    public GameObject circle;
+    public List<GameObject> dots = new List<GameObject>();
+
     private Rigidbody2D rb;
+
+    private List<Vector3Int> gridPath;
+    private int currentPathIndex;
 
     // To find a spot on or around banana
     private static int[] gridYOffsets = new int[] { -1, -1, -1,   0, 0, 0,   1, 1, 1 };
@@ -20,41 +30,71 @@ public class MonkeMove : MonoBehaviour
 
     void Start()
     {
-        targetPosition = FindPositionAroundBanana();
         rb = GetComponent<Rigidbody2D>();
+
+        gridPath = new List<Vector3Int>();
+        currentPathIndex = 0;
+
+        Vector3Int startPos = groundTilemap.WorldToCell(rb.position);
+        if (finder.UpdatePath(startPos, FindPositionAroundBanana(), ref gridPath))
+        {   // Visual Debugging
+            for (int i = 0; i  < gridPath.Count; i++)
+                dots.Add(Instantiate(circle, (Vector3) gridPath[i] + new Vector3(0.5f, 0.5f, 0f), Quaternion.identity));
+        }
     }
 
     void LateUpdate()
     {
         if (Input.GetMouseButtonDown(0))
-            targetPosition = FindPositionAroundBanana();
+        {
+            Vector3Int startPos = groundTilemap.WorldToCell(rb.position);
+            Vector3Int endPos = FindPositionAroundBanana();
+            if (finder.UpdatePath(startPos, endPos, ref gridPath))
+            {
+                currentPathIndex = 0;
+
+                {   // Visual Debugging
+                    for (int i = 0; i  < dots.Count; i++)
+                        Destroy(dots[i]);
+                    dots.Clear();
+
+                    for (int i = 0; i  < gridPath.Count; i++)
+                        dots.Add(Instantiate(circle, (Vector3) gridPath[i] + new Vector3(0.5f, 0.5f, 0f), Quaternion.identity));
+                }
+            }
+        }
     }
 
     void FixedUpdate()
     {
+        Debug.Log("Grid Path Count: " + gridPath.Count);
+        if (currentPathIndex >= gridPath.Count)
+            return;
+
+        Vector3 targetPosition = dots[currentPathIndex].transform.position;
         Vector3 move = Vector3.MoveTowards(rb.position, targetPosition, settings.moveSpeed * Time.deltaTime);
         rb.MovePosition(move);
+
+        if (((Vector3) rb.position - targetPosition).sqrMagnitude < 0.01f)
+        {
+            dots[currentPathIndex].active = false;
+            currentPathIndex++;
+        }
     }
 
-    Vector3 FindPositionAroundBanana()
+    // @Todo: Change this to return Vector3Int for pathfinding
+    Vector3Int FindPositionAroundBanana()
     {
         Vector3Int bananaGridPosition = groundTilemap.WorldToCell(bananaYumYum.position);
-        Vector3 position = (Vector3) bananaGridPosition;
 
         int offsetIndex = Random.Range(0, gridYOffsets.Length);
         for (int i = offsetIndex; i != offsetIndex - 1; i = (i + 1) % gridYOffsets.Length)
         {
             Vector3Int positionToCheck = bananaGridPosition + new Vector3Int(gridXOffsets[i], gridYOffsets[i], 0);
             if (groundTilemap.HasTile(positionToCheck) && !levelTilemap.HasTile(positionToCheck))
-            {
-                position = (Vector3) positionToCheck;
-                break;
-            }
+                return positionToCheck;
         }
 
-        position.x += 0.5f;
-        position.y += 0.5f;
-
-        return position;
+        return bananaGridPosition;
     }
 }
---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+// Custom A* pathfinder for level grid
+public class PathFinder : MonoBehaviour
+{
+    public Tilemap groundTilemap;
+    public Tilemap levelTilemap;
+
+    private HashSet<Vector3Int> closedSet = new HashSet<Vector3Int>();
+
+    private static int[] neighboursX = new int[] { -1, 0,  0, 1 };
+    private static int[] neighboursY = new int[] {  0, 1, -1, 0 };
+
+    public class Node : IHeapItem<Node>
+    {
+        public Vector3Int position;
+        public int distanceRemaining;
+        public int distanceTravelled;
+        public Node parent = null;
+        int heapIndex;
+
+        public Node(Vector3Int _position)
+        {
+            position = _position;
+        }
+
+        public int FullCost {
+            get {
+                return distanceRemaining + distanceTravelled;
+            }
+        }
+
+        public int HeapIndex
+        {
+            get
+            {
+                return heapIndex;
+            }
+            set
+            {
+                heapIndex = value;
+            }
+        }
+
+        public int CompareTo(Node other)
+        {
+            int compare = FullCost.CompareTo(other.FullCost);
+
+            if (compare == 0)
+                compare = distanceRemaining.CompareTo(other.distanceRemaining);
+
+            return -compare;
+        }
+    }
+
+    public bool UpdatePath(Vector3Int startPosition, Vector3Int endPosition, ref List<Vector3Int> gridPath)
+    {
+        Heap<Node> openSet = new Heap<Node>(groundTilemap.size.x * groundTilemap.size.y);
+        closedSet.Clear();
+
+        Node startNode = new Node(startPosition);
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Node node = openSet.Pop();
+            closedSet.Add(node.position);
+
+            // Reached end
+            if (node.position == endPosition)
+            {
+                // Walk back to fill path
+                WalkBack(node, ref gridPath);
+                return true;
+            }
+
+            for (int i = 0; i < neighboursX.Length; i++)
+            {
+                Vector3Int neighbourPosition = node.position + new Vector3Int(neighboursX[i], neighboursY[i], 0);
+
+                if (closedSet.Contains(neighbourPosition)     ||
+                    !groundTilemap.HasTile(neighbourPosition) ||
+                    levelTilemap.HasTile(neighbourPosition))
+                {
+                    continue;
+                }
+
+                int movementCost = node.distanceTravelled + GetWeightedDistance(node.position, neighbourPosition);
+
+                Node neighbourNode = new Node(neighbourPosition);
+                neighbourNode.distanceTravelled = movementCost;
+                neighbourNode.distanceRemaining = GetWeightedDistance(neighbourPosition, endPosition);
+                neighbourNode.parent = node;
+
+                if (!openSet.Contains(neighbourNode))
+                    openSet.Add(neighbourNode);
+            }
+        }
+
+        return false;
+    }
+
+    void WalkBack(Node finalNode, ref List<Vector3Int> gridPath)
+    {
+        gridPath.Clear();
+
+        Node currentNode = finalNode;
+
+        while (currentNode != null)
+        {
+            gridPath.Add(currentNode.position);
+            currentNode = currentNode.parent;
+        }
+
+        gridPath.Reverse();
+    }
+
+    // Ignores z coordinate
+    int GetWeightedDistance(Vector3Int start, Vector3Int end)
+    {
+        int xDist = Mathf.Abs(end.x - start.x);
+        int yDist = Mathf.Abs(end.y - start.y);
+
+        if (xDist > yDist)
+            return 14 * yDist + 10 * (xDist - yDist);
+
+        return 14 * xDist + 10 * (yDist - xDist);
+    }
+}
