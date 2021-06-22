17f63a9e732c3d5ccd959126aab6d372cece50ea
Hritik Dutta <dutta.hritik@gmail.com>
2021-06-22 14:28:30 +0530

    Drawing the path properly with lines between points for better visualization

--- Assets/Scripts/Editors/DemonPathEditor.cs
+++ Assets/Scripts/Editors/DemonPathEditor.cs
@@ -9,18 +9,21 @@ public class DemonPathEditor
     [DrawGizmo(GizmoType.NonSelected | GizmoType.Selected | GizmoType.Pickable)]
     public static void OnDrawGizmos(PathComponent pathObject, GizmoType gizmoType)
     {
-        if ((gizmoType & GizmoType.Selected) != 0)
+        for (int i = 0; i < pathObject.transform.childCount; i++)
         {
-            Gizmos.color = Color.red;
-        }
-        else
-        {
-            Gizmos.color = Color.yellow;
-        }
+            Transform point = pathObject.transform.GetChild(i);
 
-        foreach (Transform point in pathObject.transform)
-        {
+            Gizmos.color = Color.blue;
+
+            if (Selection.Contains(point.gameObject))
+                Gizmos.color = Color.green;
+
             Gizmos.DrawSphere(point.position, 0.1f);
+
+            Gizmos.color = Color.red;
+
+            int prevIndex = (i + pathObject.transform.childCount - 1) % pathObject.transform.childCount;
+            Gizmos.DrawLine(point.position, pathObject.transform.GetChild(prevIndex).position);
         }
     }
 }
